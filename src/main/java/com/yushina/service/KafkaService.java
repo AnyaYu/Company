d197b354027c0a39000542a851ff060011847260
annayushina <anna.yushina@gmail.com>
2023-02-26 18:55:15 +0100

    Move kafka.topic to application.properties

--- src/main/java/com/yushina/service/KafkaService.java
+++ src/main/java/com/yushina/service/KafkaService.java
@@ -2,17 +2,21 @@ package com.yushina.service;
 
 import com.yushina.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaService {
+
+    @Value("${kafka.topic}")
+    private String kafkaTopic;
     @Autowired
     private KafkaTemplate<String, Employee> kafkaTemplate;
 
     @Async
     public void sendKafkaMessage(String message, Employee employee) {
-        kafkaTemplate.send("employee-events", message, employee);
+        kafkaTemplate.send(kafkaTopic, message, employee);
     }
 }
